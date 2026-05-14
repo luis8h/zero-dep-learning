d123d076e00fc68bb48dd64b25f4a7909107bd64
luis8h <git@luis8h.de>
2026-05-14 09:46:14 +0200

    implementing end of stream error

--- libs/deflate_core/src/bits/reader.rs
+++ libs/deflate_core/src/bits/reader.rs
@@ -6,6 +6,7 @@
 
 use super::Bit;
 use super::BitResult;
+use std::fmt;
 use std::slice::Iter;
 
 pub struct BitReader<'a> {
@@ -13,6 +14,19 @@ pub struct BitReader<'a> {
     buffer: u8,
     content: Iter<'a, u8>,
 }
+
+#[derive(Debug, PartialEq)]
+pub enum BitReaderError {
+    EndOfStream
+}
+impl fmt::Display for BitReaderError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            BitReaderError::EndOfStream => write!(f, "Reached end of bitstream unexpectedly"),
+        }
+    }
+}
+impl std::error::Error for BitReaderError {}
 
 impl<'a> BitReader<'a> {
     pub fn new(content: &'a [u8]) -> Self {
@@ -24,14 +38,11 @@ impl<'a> BitReader<'a> {
     }
 
     /// Reads the next Bit of a BitStream.
-    pub fn next_bit(&mut self) -> Bit {
+    pub fn next_bit(&mut self) -> Result<Bit, BitReaderError> {
         // reset position after overflow and assign next byte as buffer
         if self.position == 0 {
             self.position = 0b00000001;
-            self.buffer = *self
-                .content
-                .next()
-                .expect("Empty buffer not handlred currently");
+            self.buffer = *self.content.next().ok_or(BitReaderError::EndOfStream)?;
         }
 
         // read current bit
@@ -40,7 +51,7 @@ impl<'a> BitReader<'a> {
         // update position for next read
         self.position <<= 1;
 
-        bit
+        Ok(bit)
     }
 
     /// Reads the next n bits from the BitStream.
@@ -49,19 +60,19 @@ impl<'a> BitReader<'a> {
     /// left byte (eg. [0b00110011, 0b11001100] -> 0b110000110011 when reading 12 bits).
     /// It returns a BitResult containing just as many u32 values as needed.
     /// When interpreting the BitResult, the length of it needs to be used to get a correct interpretation.
-    pub fn next_bits(&mut self, n: usize) -> BitResult {
+    pub fn next_bits(&mut self, n: usize) -> Result<BitResult, BitReaderError> {
         // construct output vector with just as many u32 values as needed to store a result with
         // length n
         let mut out: Vec<u32> = vec![0u32; (n + 31) / 32];
 
         for i in 0..n {
-            if self.next_bit() == Bit::One {
+            if self.next_bit()? == Bit::One {
                 // if the value is 1, or it into the output vector at the correct position
                 out[i / 32] |= 1 << (i % 32);
             }
         }
 
-        BitResult::new(out, n)
+        Ok(BitResult::new(out, n))
     }
 }
 
@@ -72,40 +83,53 @@ mod test {
     #[test]
     fn next_bit() {
         let mut reader = BitReader::new(&[0b00110011, 0b11001100, 1, 110]);
-        assert_eq!(reader.next_bit(), Bit::One);
-        assert_eq!(reader.next_bit(), Bit::One);
-        assert_eq!(reader.next_bit(), Bit::Zero);
-        assert_eq!(reader.next_bit(), Bit::Zero);
+        assert_eq!(reader.next_bit().unwrap(), Bit::One);
+        assert_eq!(reader.next_bit().unwrap(), Bit::One);
+        assert_eq!(reader.next_bit().unwrap(), Bit::Zero);
+        assert_eq!(reader.next_bit().unwrap(), Bit::Zero);
     }
 
     #[test]
     fn next_bit_over_byte_border() {
         let mut reader = BitReader::new(&[0b00110011, 0b11001100, 1, 110]);
-        assert_eq!(reader.next_bit(), Bit::One);
-        assert_eq!(reader.next_bit(), Bit::One);
-        assert_eq!(reader.next_bit(), Bit::Zero);
-        assert_eq!(reader.next_bit(), Bit::Zero);
-        assert_eq!(reader.next_bit(), Bit::One);
-        assert_eq!(reader.next_bit(), Bit::One);
-        assert_eq!(reader.next_bit(), Bit::Zero);
-        assert_eq!(reader.next_bit(), Bit::Zero);
-        assert_eq!(reader.next_bit(), Bit::Zero);
-        assert_eq!(reader.next_bit(), Bit::Zero);
-        assert_eq!(reader.next_bit(), Bit::One);
-        assert_eq!(reader.next_bit(), Bit::One);
+        assert_eq!(reader.next_bit().unwrap(), Bit::One);
+        assert_eq!(reader.next_bit().unwrap(), Bit::One);
+        assert_eq!(reader.next_bit().unwrap(), Bit::Zero);
+        assert_eq!(reader.next_bit().unwrap(), Bit::Zero);
+        assert_eq!(reader.next_bit().unwrap(), Bit::One);
+        assert_eq!(reader.next_bit().unwrap(), Bit::One);
+        assert_eq!(reader.next_bit().unwrap(), Bit::Zero);
+        assert_eq!(reader.next_bit().unwrap(), Bit::Zero);
+        assert_eq!(reader.next_bit().unwrap(), Bit::Zero);
+        assert_eq!(reader.next_bit().unwrap(), Bit::Zero);
+        assert_eq!(reader.next_bit().unwrap(), Bit::One);
+        assert_eq!(reader.next_bit().unwrap(), Bit::One);
+    }
+
+    #[test]
+    fn next_bit_end_of_stream_error() {
+        let mut reader = BitReader::new(&[0]);
+
+        for i in 0..8 {
+            assert_eq!(reader.next_bit().unwrap(), Bit::Zero);
+        }
+
+        let result = reader.next_bit();
+
+        assert_eq!(result, Err(BitReaderError::EndOfStream));
     }
 
     #[test]
     fn next_bits() {
         let mut reader = BitReader::new(&[0b00110011, 0b11001100]);
-        assert_eq!(dbg!(reader.next_bits(4)), BitResult::new(vec![0b0011], 4));
+        assert_eq!(dbg!(reader.next_bits(4)).unwrap(), BitResult::new(vec![0b0011], 4));
     }
 
     #[test]
     fn next_bits_over_byte_border() {
         let mut reader = BitReader::new(&[0b00110011, 0b11001100]);
         assert_eq!(
-            reader.next_bits(12),
+            reader.next_bits(12).unwrap(),
             BitResult::new(vec![0b110000110011], 12)
         );
     }
@@ -122,6 +146,13 @@ mod test {
             0x00000003, // The next 2 bits (from the 5th byte)
         ];
 
-        assert_eq!(result, BitResult::new(expected_vec, 34));
+        assert_eq!(result.unwrap(), BitResult::new(expected_vec, 34));
+    }
+
+    #[test]
+    fn next_bits_end_of_stream_error() {
+        let mut reader = BitReader::new(&[0, 1]);
+        let result = reader.next_bits(17);
+        assert_eq!(result, Err(BitReaderError::EndOfStream));
     }
 }
